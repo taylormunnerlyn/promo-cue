f788134c0354ff5bcfbfa46b3b74ed1872374689
Taylor Munnerlyn <taylormunnerlyn@gmail.com>
2016-07-06 11:01:40 -0600

    changing files

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: process.env.PORT || 3000,
   MONGO_URI: process.env.MONGO_URI,
-  session_secret: process.env.SECRET,
-  sid: process.env.SID,
-  authToken: process.env.AUTH_TOKEN
+  session_secret: 'I love poopies and light tan',
+  sid: 'AC2a38cfb66b31b966b3767a6e9b06681d',
+  authToken: '4d7df98dbf7145a350cdc86f0676912e'
 }
