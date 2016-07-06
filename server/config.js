3b8d9f09ea5ad2fd8f8f46365032500f060330a7
taylormunnerlyn <taylormunnerlyn@gmail.com>
2016-07-06 12:12:23 -0600

    Delete config.js

--- server/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  port: 3000,
-  MONGO_URI: 'mongodb://localhost/promoCueApp',
-  session_secret: 'I love poopies and light tan',
-  sid: 'AC2a38cfb66b31b966b3767a6e9b06681d',
-  authToken: '4d7df98dbf7145a350cdc86f0676912e'
-}
